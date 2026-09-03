01b86ebd36b8f0085785bee1c33ea17b31278c5d
med-am1n <237815717+med-am1n@users.noreply.github.com>
2026-09-03 01:14:13 +0100

    Revert "update back brush cursor mousePos to use screen space"

--- src/main.cpp
+++ src/main.cpp
@@ -586,7 +586,7 @@ int main()
         {
             double xpos, ypos;
             glfwGetCursorPos(window, &xpos, &ypos);
-            glm::vec2 mousePos((float)xpos, (float)ypos);
+            glm::vec2 mousePos = camera.ScreenToWorld(glm::vec2((float)xpos,(float)ypos), windowWidth, windowHeight);
             glm::vec4 cursorColor = (currentmode == Mode::Draw) ? brushColor : glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
             DrawToolCursor(shader, cursorMesh, mousePos, brushRadius, cursorColor);
         }
